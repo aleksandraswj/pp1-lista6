153dd017b574cf513aca5862b749a6544da26e50
aleksandraswj <141584484+aleksandraswj@users.noreply.github.com>
2023-11-25 20:41:27 +0100

    Program 3.2.1

---
+++ pp1-lista6/Prog3_2_1/Prog3_2_1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int minWartosc(int a, int b)
+{
+	if (a < b)
+		return a;
+	else
+		return b;
+} 	
+
+int minReferencja(const int& a, const  int& b)
+{
+	if (a < b)
+		return a;
+	else
+		return b;
+} 
+
+int minWskaznik(const int* a, const int* b)
+{
+	if (*a < *b)
+		return *a;
+	else
+		return *b;
+} 
+
+
+int main()
+{
+	int a = 5;
+	int b = 10;	
+    std::cout << minWskaznik(&a, &b);
+}
+
